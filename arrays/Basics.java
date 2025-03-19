3334553bd58ceabea3d071f51b860e2755c0c683
devxtushar <devxtushar@gmail.com>
2025-03-19 20:20:48 +0530

    Arrays basics

---
+++ arrays/Basics.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Basics{
+    public static void main(String args[]){
+      // Takes arrays as input
+      int prices[] = new int[4];   
+
+     System.out.println("Enter prices");
+
+     Scanner sc = new Scanner(System.in);
+      prices[0]=sc.nextInt();
+      prices[1]=sc.nextInt();
+      prices[2]=sc.nextInt();
+
+      System.out.println("length : "+ prices.length);
+
+      // Takes as like objects
+    // String names[] = {"Tushar'","prateek","test","engineer"};
+
+    // System.out.println("names are:" + names[1]);
+
+    }
+
+}
