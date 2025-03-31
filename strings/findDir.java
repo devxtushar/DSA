55f37b05d378f476dc066609051cfcc8328c0ac5
devxtushar <devxtushar@gmail.com>
2025-03-31 12:30:13 +0530

    # String questions

---
+++ strings/findDir.java
@@ -0,0 +1,29 @@
+package strings;
+
+public class findDir {
+
+    public static void shortestPath(String path){
+        int x=0,y=0;
+
+        for(int i=0; i<path.length();i++){
+            char dir = path.charAt(i);
+
+            if(dir == 'S'){
+                y--;
+            } else if(dir == 'N'){
+                y++;
+            }else if(dir == 'E'){
+                x++;
+            }else{
+                x--;
+            }
+
+        }
+        System.out.println("Direction value :" + Math.sqrt(x*x + y*y));
+
+    }
+    public static void main(String[] args) {
+        String direction = "WNEENESENNN";
+        shortestPath(direction);
+    }
+}
