c1cd3128d534215df50d28b9057bcc6f39c99faa
devxtushar <devxtushar@gmail.com>
2025-04-01 08:00:58 +0530

    # string compression question

--- strings/toupperCase.java
+++ strings/toupperCase.java
@@ -17,8 +17,33 @@ public class toupperCase {
         }
         return sb.toString();
     }
+
+    public static String compressString(String str){
+        String newStr = ""; 
+    
+
+        for(int i=0; i < str.length() ; i++){
+            Integer count = 1;
+
+            while( i< str.length()-1 &&  str.charAt(i) == str.charAt(i+1) ){
+                count++;
+                i++;
+            }
+               newStr +=  str.charAt(i);
+               if(count > 1){
+                newStr += count.toString();
+               }
+        
+        }
+
+
+        return newStr;
+    }
     public static void main(String[] args) {
         String str = "Hi, this is devxtushar";
-        System.out.println(toUpperCaseString(str));
+        String compress = "aaabbbccaaabbc";
+        // System.out.println(toUpperCaseString(str));
+        System.out.println((compressString(compress)));
+
     }
 }
