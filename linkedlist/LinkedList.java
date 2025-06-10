a2323675dd3a5699042f77b5b3850e9e9db67e21
devxtushar <devxtushar@gmail.com>
2025-06-10 09:07:56 +0530

    # linked list

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,66 @@
+package linkedlist;
+
+public class LinkedList {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data){
+           // s1 - create a new node
+        Node newNode = new Node(data);
+        if(head==null) {
+            head = tail= newNode;
+            return; 
+        }
+        // s2 - newNode next = head
+        newNode.next = head;
+        // s3 - head = newNode
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+
+    }
+    public void print(){
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp=head;
+        while(temp !=null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll= new LinkedList();
+        // ll.head = new Node(1);
+        // ll.head.next =  new Node(2);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1); 
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+
+        
+    }
+    
+}
