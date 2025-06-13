4a8ef20e8a38c9dde94744d871692951924ae05d
devxtushar <devxtushar@gmail.com>
2025-06-13 21:28:20 +0530

    # linked list updated

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -11,9 +11,11 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
     public void addFirst(int data){
            // s1 - create a new node
         Node newNode = new Node(data);
+        size++;
         if(head==null) {
             head = tail= newNode;
             return; 
@@ -25,6 +27,7 @@ public class LinkedList {
     }
     public void addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head=tail=newNode;
             return;
@@ -45,20 +48,36 @@ public class LinkedList {
         }
         System.out.println("null");
     }
+    public void add(int idx,int data){ //middle - o(n) takes, because I have to find the index in LL but constant time for operations
+        //and in array index is known.
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+          Node newNode = new Node(data);
+          size++;
+          Node temp = head;
+          int i=0;
+          while(i<idx-1){
+            temp=temp.next;
+            i++;
+          }
+          newNode.next = temp.next;
+          temp.next = newNode;
+            
+    }
 
     public static void main(String[] args) {
         LinkedList ll= new LinkedList();
         // ll.head = new Node(1);
         // ll.head.next =  new Node(2);
-        ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1); 
-        ll.print();
         ll.addLast(3);
-        ll.print();
         ll.addLast(4);
-        ll.print();
+        ll.add(2,9);
+        ll.print(); 
+        System.out.println(ll.size);
 
         
     }
