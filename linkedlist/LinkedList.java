bfbf45de5aa5e1d2d719ff62e168f82e9769239a
devxtushar <devxtushar@gmail.com>
2025-06-21 08:47:47 +0530

    ll updates

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -52,7 +52,7 @@ public class LinkedList {
         //and in array index is known.
         if(idx == 0){
             addFirst(data);
-            return;
+            return; 
         }
           Node newNode = new Node(data);
           size++;
@@ -66,6 +66,40 @@ public class LinkedList {
           temp.next = newNode;
             
     }
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val = head.data;
+            head =tail=null;
+            size=0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        return val;
+    }
+      public int removeLast(){// not working
+            if(size==0){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val = head.data; // head and tail both are same
+            head =tail=null;
+            size=0; 
+            return val;
+        }
+        Node prev= head;
+       for(int i=0;i<size-2;i++){
+        prev=prev.next;
+       } 
+       int val =tail.data;// tail.data
+       prev.next=null;
+       tail=prev;
+       size--;
+       return val;
+    }
 
     public static void main(String[] args) {
         LinkedList ll= new LinkedList();
@@ -78,6 +112,14 @@ public class LinkedList {
         ll.add(2,9);
         ll.print(); 
         System.out.println(ll.size);
+        ll.removeFirst();
+        ll.print(); 
+        ll.removeLast();
+        ll.print();  
+        System.out.println(ll.size);
+
+
+
 
         
     }
