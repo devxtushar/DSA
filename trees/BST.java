43c92f03e8c169b13a901eb2e48f672e8d48c020
devxtushar <devxtushar@gmail.com>
2025-07-25 08:04:37 +0530

    # binary search tree search and delete node

--- trees/BST.java
+++ trees/BST.java
@@ -7,7 +7,7 @@ public class BST {
         Node right;
 
         Node(int data) {
-            this.data = data;
+            this.data = data; // left and rgt is already null
         }
     }
 
@@ -35,12 +35,67 @@ public class BST {
         inorder(root.right);
     }
 
+    public static boolean search(Node root, int key) { // o(H)
+        if (root == null) {
+            return false;
+        }
+        if (root.data == key) {
+            return true;
+        }
+        if (root.data > key) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static Node delete(Node root, int val) {
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case 1 - leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case 2 - single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            // case 3 - both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
         inorder(root);
+        System.out.println();
+        if (search(root, 1)) {
+            System.out.println("found");
+        } else {
+            System.out.println("no found");
+        }
+        root = delete(root, 5);
+        System.out.println();
+        inorder((root));
     }
 }
